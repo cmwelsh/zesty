0af1ec56fe84000b137884ee5a4eb238ec787c3a
cmwelsh <chris@cmwelsh.com>
2012-11-06 17:02:39 -0700

    add page template to redirect to child page

---
+++ page-templates/redirect-child.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Template Name: Redirect to Child Page
+*/
+
+$query = new WP_Query(array(
+  'post_type' => 'page',
+  'posts_per_page' => 1,
+  'order' => 'ASC',
+  'orderby' => 'menu_order',
+  'post_parent' => get_the_ID(),
+));
+
+if ($query->have_posts()) {
+  while ($query->have_posts()) {
+    $query->the_post();
+
+    header("Location: " . get_permalink());
+    exit;
+  }
+}
