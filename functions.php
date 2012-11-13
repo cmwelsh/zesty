cf9847d10b02118609488fef92d14fa56a6937be
cmwelsh <chris@cmwelsh.com>
2012-11-13 16:38:13 -0700

    add favicon support

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@ function zesty_base_init() {
 	Zesty::load_snippet('facebook-open-graph');
 	Zesty::load_snippet('remove-meta-tags');
 	Zesty::load_snippet('new-page-menu-order');
+	Zesty::load_snippet('favicon');
 }
 add_action('after_setup_theme', 'zesty_base_init');
 
---
+++ inc/favicon.php
@@ -0,0 +1,14 @@
+<?php
+namespace Snippet;
+
+class Favicon {
+    public function __construct() {
+        add_action('wp_head', array($this, 'render_favicon'));
+        add_action('admin_head', array($this, 'render_favicon'));
+    }
+    public function render_favicon() {
+        ?>
+        <link rel="shortcut icon" href="<?= get_stylesheet_directory_uri() ?>/assets/images/favicon.png">
+        <?php
+    }
+}
