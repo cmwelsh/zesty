cfc548a2a4a9a9b65bc26fb0e847b27b4122f0f2
cmwelsh <chris@cmwelsh.com>
2012-11-30 01:48:39 -0700

    fix Nav Menu Fallback to home link not working

--- includes/nav-menu-fallback.php
+++ includes/nav-menu-fallback.php
@@ -1,12 +1,15 @@
 <?php
 namespace Snippet;
 
+// If theme location menu does not exist, show a basic link to the Home page
+// instead of listing all pages
+
 class Nav_Menu_Fallback {
-
+    public function __construct() {
+        add_filter('wp_page_menu_args', array($this, 'wp_page_menu_args'));
+    }
+    public function wp_page_menu_args($args) {
+        $args['show_home'] = true;
+        return $args;
+    }
 }
-
-function twentytwelve_page_menu_args( $args ) {
-    $args['show_home'] = true;
-    return $args;
-}
-add_filter( 'wp_page_menu_args', 'twentytwelve_page_menu_args' );
